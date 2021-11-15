032303a2761d2701f6920ce7881b58676f3bcf17
Or Kravitz <imorkravitz@gmail.com>
2021-11-15 09:48:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tictactoe_application_mission_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tictactoe_application_mission_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
